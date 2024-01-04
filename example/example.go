ae587f093b4e54467a45a3cd16cdc515248449a6
marubontan <marubontan@gmail.com>
2024-01-04 11:46:25 -0300

    adding env step

--- example/example.go
+++ example/example.go
@@ -12,6 +12,11 @@ func main() {
 	maze.SetGoal(2, 2)
 	maze.SetObstacle(1, 1)
 	maze.SetObstacle(0, 1)
+	maze.SetTrap(2, 1)
+	maze.SetGoalReward(1.0)
+	maze.SetTrapPenalty(-0.1)
 	maze.Print()
 	fmt.Printf("Path from start to goal: %t\n", maze.ExistPath())
+	fmt.Printf("Goal Reward: %f\n", *maze.GoalReward)
+	fmt.Printf("Trap Penalty: %f\n", *maze.TrapPenalty)
 }
--- maze/maze.go
+++ maze/maze.go
@@ -8,6 +8,7 @@ import (
 const (
 	Empty = iota
 	Obstacle
+	Trap
 	Start
 	Goal
 )
@@ -24,9 +25,12 @@ type Block struct {
 	Attributes map[string]any
 }
 type Maze struct {
-	Height int
-	Width  int
-	Blocks [][]Block
+	Height      int
+	Width       int
+	Blocks      [][]Block
+	GoalReward  *float64
+	TrapPenalty *float64
+	State       *[2]int
 }
 
 func NewMaze(h, w int) *Maze {
@@ -68,6 +72,14 @@ func (m *Maze) SetGoal(x, y int) error {
 	return nil
 }
 
+func (m *Maze) SetGoalReward(reward float64) {
+	m.GoalReward = &reward
+}
+
+func (m *Maze) SetTrapPenalty(penalty float64) {
+	m.TrapPenalty = &penalty
+}
+
 func (m *Maze) GetGoal() (int, int, error) {
 	for y := 0; y < m.Height; y++ {
 		for x := 0; x < m.Width; x++ {
@@ -85,6 +97,50 @@ func (m *Maze) SetObstacle(x, y int) error {
 	}
 	m.Blocks[y][x] = Block{BlockType: Obstacle}
 	return nil
+}
+
+func (m *Maze) SetTrap(x, y int) error {
+	if x < 0 || x >= m.Width || y < 0 || y >= m.Height {
+		return errors.New("invalid goal position")
+	}
+	m.Blocks[y][x] = Block{BlockType: Trap}
+	return nil
+}
+
+func (m *Maze) Step(action int) ([2]int, bool, float64, error) {
+	var nextStateCandidate [2]int
+	switch action {
+	case Right:
+		nextStateCandidate = [2]int{m.State[0] + 1, m.State[1]}
+	case Down:
+		nextStateCandidate = [2]int{m.State[0], m.State[1] + 1}
+	case Left:
+		nextStateCandidate = [2]int{m.State[0] - 1, m.State[1]}
+	case Up:
+		nextStateCandidate = [2]int{m.State[0], m.State[1] - 1}
+	}
+	if !m.IsAvailable(nextStateCandidate[0], nextStateCandidate[1]) {
+		return *m.State, false, 0, nil
+	}
+	reward := 0.0
+	isGoal := false
+	if m.Blocks[nextStateCandidate[1]][nextStateCandidate[0]].BlockType == Goal {
+		reward = *m.GoalReward
+		isGoal = true
+	} else if m.Blocks[nextStateCandidate[1]][nextStateCandidate[0]].BlockType == Trap {
+		reward = *m.TrapPenalty
+	}
+	m.State = &nextStateCandidate
+	return nextStateCandidate, isGoal, reward, nil
+}
+
+func (m *Maze) Reset() {
+	x, y, err := m.GetStart()
+	if err != nil {
+		x, y = 0, 0
+		m.SetStart(x, y)
+	}
+	m.State = &[2]int{x, y}
 }
 
 func (m *Maze) IsAvailable(x, y int) bool {
@@ -114,6 +170,8 @@ func (m *Maze) Print() {
 				fmt.Print(" |")
 			case Obstacle:
 				fmt.Print("X|")
+			case Trap:
+				fmt.Print("T|")
 			case Start:
 				fmt.Print("S|")
 			case Goal:
